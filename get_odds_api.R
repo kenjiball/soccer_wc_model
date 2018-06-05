d9b68c2b136dfe7c0157f5b620941cae7b5a23e7
kenjiball <patrick.kenji.ball@gmail.com>
2018-06-05 17:36:35 +1000

    Created first version of odds data api pull for soccer wc

--- get_odds_api.R
+++ get_odds_api.R
@@ -17,20 +17,34 @@ grabData <- function(gameid,api_url,api_key){
    # Call API to get data for a game
   jsonData <- fromJSON(API_str)
   #jsonDataPretty <- toJSON(jsonData, pretty = TRUE)
-  return(jsonData)
+  jsonData <- as.data.frame(jsonData)
+  names(jsonData) <- gsub("odds.","",names(jsonData))
+  
+  event_id <- rep(gameid,nrow(jsonData))
+  price_amt <- jsonData$price$value
+  price_mvmt <- jsonData$price$movement
+  jsonData2 <- data.frame(event_id,jsonData$selectionId, jsonData$type
+                          , jsonData$betType, price_amt, price_mvmt, jsonData$bookmakerId )
+  names(jsonData2) <- gsub("jsonData.","",names(jsonData2))
+  return(jsonData2)
 }
 
-
-api_url_wc <- "https://puntapi.com/odds/event/"
-api_odds_key <- "?derived=best&betTypes=fixed-win" 
-
-
+# set game id vector
 game_id <- c(658072:658102,658104:658120)
-game_id
-length(odds_id)
 
 # Single game 
 grabData(658072,api_url_wc,api_odds_key)
 
+# loop through all game ids
+all_match_data <- data.frame()
+  
+for(i in 1:length(game_id)){
 
+  event_data <- grabData(game_id[i],api_url_wc,api_odds_key)
+  
+  all_match_data <- rbind(all_match_data,event_data)
+  
+}
+
+all_match_data
 
