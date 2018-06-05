2541cd356609f73310710e9008aab35875be5bcf
kenjiball <patrick.kenji.ball@gmail.com>
2018-06-05 15:24:02 +1000

    Initial commit

---
+++ get_odds_api.R
@@ -0,0 +1,40 @@
+https://puntapi.com/odds/event/658072?derived=best&betTypes=fixed-win
+
+odds_id <- 658072:658120
+odds_id <- odds_id[-658103]
+-658103
+
+{  
+  "odds":[  
+    {  
+      "selectionId":6593441,
+      "type":"best",
+      "betType":"fixed-win",
+      "price":{  
+        "value":19.75,
+        "movement":-0.25
+      },
+      "bookmakerId":"sportsbetting"
+    },
+    {  
+      "selectionId":6593440,
+      "type":"best",
+      "betType":"fixed-win",
+      "price":{  
+        "value":6.5,
+        "movement":-0.1
+      },
+      "bookmakerId":"sportsbet"
+    },
+    {  
+      "selectionId":6593439,
+      "type":"best",
+      "betType":"fixed-win",
+      "price":{  
+        "value":1.2,
+        "movement":-0.05
+      },
+      "bookmakerId":"ladbrokes"
+    }
+    ]
+}
